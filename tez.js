26d3f74cf185cbbfce3900cae5a90e3bbea09c03
Jonathan <vanillajonathan@users.noreply.github.com>
2021-05-20 10:20:31 +0200

    Format code

--- tez.js
+++ tez.js
@@ -9,19 +9,14 @@ export const qsa = (sel, ctx = doc) => ctx.querySelectorAll(sel);
 export const style = (el, obj) => Object.assign(el.style, obj);
 
 export const attr = (el, name, val) => {
+  if (val == null)
+    return el.getAttribute(name);
   
-  if(val == null) return el.getAttribute(name);
-  
-  if(val == false) {
-    
+  if (val == false) {
     el.removeAttribute(name);
-  
   } else {
-    
     el.setAttribute(name, val);
-  
   }
-
 };
 
 export const on = (el, evt, hand) => el.addEventListener(evt, hand, false);
@@ -29,15 +24,9 @@ export const on = (el, evt, hand) => el.addEventListener(evt, hand, false);
 export const off = (el, evt, hand) => el.removeEventListener(evt, hand, false);
 
 export const ready = app => {
-
   if (/complete|loaded|interactive/.test(doc.readyState) && doc.body) {
-
     setTimeout(app, 1);
-
   } else {
-
     on(doc, 'DOMContentLoaded', app);
-
   }
-
 };
