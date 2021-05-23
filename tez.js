0f332ae9cb5db0bd63a3841f192827dd957234d4
rwbeast <64768386+rwbeast@users.noreply.github.com>
2021-05-23 13:33:18 +0530

    Update tez.js

--- tez.js
+++ tez.js
@@ -11,7 +11,7 @@ const parseSelector = selector => {
 
   let id = '', className = '';
 
-  for(let i = 1; i < tokens.length; ++i) {
+  for(let i = 1; i < tokens.length; i += 2) {
     switch(tokens[i]) {
       case '.':
         className += ` ${tokens[i + 1]}`;
